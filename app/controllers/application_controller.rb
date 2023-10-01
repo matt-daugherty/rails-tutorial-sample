eca9fafea30ebfbae6566ae1545e0eb561beafd4
Matt Daugherty <matt@mattdaugherty.me>
2023-10-01 19:01:03 -0400

    Improved Home and Help page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,2 @@
 class ApplicationController < ActionController::Base
-    def test
-        render html: "it's working!"
-    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "application#test"
+  root "static_pages#home"
 end
